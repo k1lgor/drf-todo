7bb48a7a020d4126f9af40f8547a803da131c3ff
Plamen Ivanov <plamen.ivanov@strypes.eu>
2023-12-09 20:04:35 +0200

    chore(admin.py): add docstring to TodoAdmin class

--- todo/admin.py
+++ todo/admin.py
@@ -4,6 +4,17 @@ from .models import Todo
 
 
 class TodoAdmin(admin.ModelAdmin):
+    """
+    Admin model for Todo.
+
+    Args:
+        self: The TodoAdmin instance.
+
+    Attributes:
+        readonly_fields (tuple): Fields that are read-only in the admin interface.
+
+    """
+
     readonly_fields = ("created",)
 
 
