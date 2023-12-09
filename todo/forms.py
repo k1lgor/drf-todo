fb1bdc203626a96944af5df7c8ebba97b6b2fcf4
Plamen Ivanov <plamen.ivanov@strypes.eu>
2023-12-09 20:05:03 +0200

    chore(forms.py): add docstring to TodoForm class

--- todo/forms.py
+++ todo/forms.py
@@ -4,6 +4,19 @@ from .models import Todo
 
 
 class TodoForm(ModelForm):
+    """
+    Form for creating or updating a Todo.
+
+    Args:
+        self: The TodoForm instance.
+
+    Attributes:
+        Meta (class): Inner class that defines the metadata options for the form.
+            - model (Model): The model class associated with the form.
+            - fields (list): The fields to include in the form.
+
+    """
+
     class Meta:
         model = Todo
         fields = ["title", "memo", "important"]
