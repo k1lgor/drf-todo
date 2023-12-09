887cd31e87a19137036aec02570bbf8734155e1d
Plamen Ivanov <plamen.ivanov@strypes.eu>
2023-12-09 20:05:33 +0200

    docs(models.py): add docstring to Todo model class

--- todo/models.py
+++ todo/models.py
@@ -3,6 +3,22 @@ from django.db import models
 
 
 class Todo(models.Model):
+    """
+    Model representing a Todo item.
+
+    Attributes:
+        title (CharField): The title of the Todo item.
+        memo (TextField): The memo or description of the Todo item.
+        created (DateTimeField): The date and time when the Todo item was created.
+        datecompleted (DateTimeField): The date and time when the Todo item was completed.
+        important (BooleanField): Indicates whether the Todo item is important or not.
+        user (ForeignKey): The user associated with the Todo item.
+
+    Methods:
+        __str__(): Returns a string representation of the Todo item.
+
+    """
+
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
